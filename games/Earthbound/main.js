195fb144e6cb1faa4a59154df49a67b154c43fc3
geodome21 <gibson.h.matt@gmail.com>
2025-12-11 23:13:16 +0000

    EARHTINHG BOUNDINGE

--- games/Earthbound/main.js
+++
@@ -1,7 +0,0 @@
-//Service Worker
-if('serviceWorker' in navigator) {
-    console.log('Puedes usar el service worker en tu navegador');
-    navigator.serviceWorker.register('sw.js');
-} else {
-    console.log('No puede usar los service worker en tu navegador');
-}
