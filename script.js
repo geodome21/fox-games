dad96c22af5e7bfb9080479e5a037a69708eaeed
geodome21 <gibson.h.matt@gmail.com>
2025-12-12 18:09:54 +0000

    fijofe

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const games = [
     { name: "Geometry dash", url: "games/Geometry dash/index.html" },
     { name: "Minesweeper", url: "games/Minesweeper/index.html" },
     { name: "nba jam", url: "games/nba jam/index.html" },
-
+    { name: "furious racing 3D", url: "games/furious racing 3D/index.html" },
 ];  
 
 // (No background animation) Keep script focused on game list and theme toggle
