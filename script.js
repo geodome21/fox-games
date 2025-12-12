d6313410987d7162f8032d083df658219e8e8aca
geodome21 <gibson.h.matt@gmail.com>
2025-12-12 17:27:28 +0000

    a[wf]

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const games = [
     { name: "Gunspin", url: "games/Gunspin/index.html" },
     { name: "Polytrack", url: "games/Polytrack/index.html" },
     { name: "Slope", url: "games/Slope/index.html" },
-    { name: "1v1.lol", url: "games/1v1 lol/index.html" },
+    { name: "1v1.lol", url: "games/1v1.lol/index.html" },
     { name: "Geometry dash", url: "games/Geometry dash/index.html" },
     { name: "Minesweeper", url: "games/Minesweeper/index.html" },
 
