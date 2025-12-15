5fae99cb2b265b78b19d6694d317ae05f1dd5243
geodome21 <gibson.h.matt@gmail.com>
2025-12-15 19:47:55 +0000

    ohAEFa

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ const games = [
     { name: "Geometry dash", url: "games/Geometry dash/index.html" },
     { name: "Minesweeper", url: "games/Minesweeper/index.html" },
     { name: "nba jam", url: "games/nba jam/index.html" },
-    { name: "furious racing 3D", url: "games/furious racing 3D/index.html" },
     { name: "Stick Merge", url: "games/Stick Merge/index.html" },
 ];  
 
