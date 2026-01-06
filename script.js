3c5541205d542cae86905163f48e931c7ca53af9
geodome21 <gibson.h.matt@gmail.com>
2026-01-06 17:17:11 +0000

    added fnaf 2

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ const games = [
     { name: "Wordle", url: "games/Wordle/index.html" },
     { name: "Super Smash Bros 64", url: "games/Super Smash Bros 64/index.html" },
     { name: "Papas Pizzaria", url: "games/Papas pizzaria/index.html" },
+    { name: "Five Nights at Freddys 2", url: "games/five nights at freddys 2/index.html" },
 ]
 
 
