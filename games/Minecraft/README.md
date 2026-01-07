c5858752d11db7d81f77a0b06de072acc2a4d540
geodome21 <gibson.h.matt@gmail.com>
2026-01-07 17:26:59 +0000

    removed BS r#	modified:   script.js

--- games/Minecraft/README.md
+++
@@ -1,8 +0,0 @@
-<div align='center'>
-
-# Klockcraft 0.30
-## This is the actual version of Minecraft 0.30, feel free to fork, or run it for yourself.
-### ⚠️ WARNING: Please be careful and responsible with this software because it is a legit copy of the game!
-### 🟦 TIP: Do not run this project locally, please run on a live server or HTTP.
-![proof](https://raw.githubusercontent.com/Klockcraft-Official/.github/main/2023-11-19.png)
-### Happy Hacking! 💻
--- script.js
+++ script.js
@@ -47,7 +47,6 @@ const games = [
     { name: "Learn to fly", url: "games/learn to fly/index.html" },
     { name: "Learn to fly 3", url: "games/learn to fly 3/index.html" },
     { name: "level devil", url: "games/level devil/index.html" },
-    { name: "Minecraft 0.30", url: "games/Minecraft/index.html" },
     { name: "Minecraft 1.8.8", url: "games/Minecraft 1.8.8/index.html" },
     { name: "Minesweeper", url: "games/Minesweeper/index.html" },
     { name: "nba jam", url: "games/nba jam/index.html" },
